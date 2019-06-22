1b38f3c5a45097482fea688011e0aa8f07da2dc4
Lucas <lorsi@itba.edu.ar>
2019-06-22 15:35:39 -0300

    Added file

--- main/single_phase_inverter.c
+++ main/single_phase_inverter.c
@@ -1,18 +1,3 @@
-/* brushed dc motor control example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
-/*
- * This example will show you how to use MCPWM module to control brushed dc motor.
- * This code is tested with L298 motor driver.
- * User may need to make changes according to the motor driver they use.
-*/
-
 #include <stdio.h>
 
 #include "freertos/FreeRTOS.h"
@@ -23,8 +8,11 @@
 #include "soc/mcpwm_reg.h"
 #include "soc/mcpwm_struct.h"
 
-#define GPIO_PWM0A_OUT 15   //Set GPIO 15 as PWM0A
-#define GPIO_PWM0B_OUT 16   //Set GPIO 16 as PWM0B
+/* Pin Definitions*/
+#define GPIO_PWM0A_OUT 15 //Set GPIO 15 as PWM0A
+#define GPIO_PWM0B_OUT 16 //Set GPIO 16 as PWM0B
+
+const uint8_t debug_table[] = {0, 512, 1023, 512};
 
 static void mcpwm_example_gpio_initialize()
 {
@@ -36,7 +24,7 @@ static void mcpwm_example_gpio_initialize()
 /**
  * @brief motor moves in forward direction, with duty cycle = duty %
  */
-static void brushed_motor_forward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle)
+static void brushed_motor_forward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num, float duty_cycle)
 {
     mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_B);
     mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_A, duty_cycle);
@@ -46,11 +34,11 @@ static void brushed_motor_forward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_nu
 /**
  * @brief motor moves in backward direction, with duty cycle = duty %
  */
-static void brushed_motor_backward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle)
+static void brushed_motor_backward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num, float duty_cycle)
 {
     mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_A);
     mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_B, duty_cycle);
-    mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_B, MCPWM_DUTY_MODE_0);  //call this each time, if operator was previously in low/high state
+    mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_B, MCPWM_DUTY_MODE_0); //call this each time, if operator was previously in low/high state
 }
 
 /**
@@ -73,13 +61,14 @@ static void mcpwm_example_brushed_motor_control(void *arg)
     //2. initial mcpwm configuration
     printf("Configuring Initial Parameters of mcpwm...\n");
     mcpwm_config_t pwm_config;
-    pwm_config.frequency = 1000;    //frequency = 500Hz,
-    pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
-    pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
+    pwm_config.frequency = 1000; //frequency = 500Hz,
+    pwm_config.cmpr_a = 0;       //duty cycle of PWMxA = 0
+    pwm_config.cmpr_b = 0;       //duty cycle of PWMxb = 0
     pwm_config.counter_mode = MCPWM_UP_COUNTER;
     pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
-    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);    //Configure PWM0A & PWM0B with above settings
-    while (1) {
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config); //Configure PWM0A & PWM0B with above settings
+    while (1)
+    {
         brushed_motor_forward(MCPWM_UNIT_0, MCPWM_TIMER_0, 50.0);
         vTaskDelay(2000 / portTICK_RATE_MS);
         brushed_motor_backward(MCPWM_UNIT_0, MCPWM_TIMER_0, 30.0);
