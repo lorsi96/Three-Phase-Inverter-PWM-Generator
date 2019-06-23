524325b508abed3419dd91d2396fc0223203ee44
Lucas <lorsi@itba.edu.ar>
2019-06-23 14:48:24 -0300

    Ready for single phase testing

--- main/main.c
+++ main/main.c
@@ -5,16 +5,17 @@
 #include "esp_attr.h"
 
 #include "extended_mcpwm.h"
-//#include "driver/mcpwm.h"
 #include "soc/mcpwm_reg.h"
 #include "soc/mcpwm_struct.h"
 
-#define LOG_LOCAL_LEVEL ESP_LOG_VERBOSE
 #include "esp_log.h"
 
 /* Pin Definitions*/
 #define GPIO_PWM0A_OUT 15 //Set GPIO 15 as PWM0A
 #define GPIO_PWM0B_OUT 16 //Set GPIO 16 as PWM0B
+
+/* Const Definitions */
+#define CUSTOM_DEADTIME 1000 //In ns
 
 static const char *TAG = "SinglePhase";
 const double debug_table[] = {1.0, 50.0, 99.0, 50.0};
@@ -35,7 +36,6 @@ static void update_pwm(const double *table, const uint8_t tab_len, const mcpwm_u
     /* Output next sample */
     static uint8_t table_pointer = 0;
     mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_A, table[table_pointer]);
-    mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_B, table[table_pointer]);
     ++table_pointer;
     table_pointer %= tab_len;
 }
@@ -53,10 +53,8 @@ static void main_loop(void *arg)
     pwm_config.cmpr_b = 0;       //duty cycle of PWMxb = 0
     pwm_config.counter_mode = MCPWM_UP_COUNTER;
     pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
-    mcpwm_deadtime_enable(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_ACTIVE_HIGH_COMPLIMENT_MODE, 1000, 1000); //10us
-    //mcpwm_set_duty_type(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, MCPWM_DUTY_MODE_0);
-    //mcpwm_set_duty_type(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, MCPWM_DUTY_MODE_1);
-    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config); //Configure PWM0A & PWM0B with above settings
+    mcpwm_deadtime_enable(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_ACTIVE_HIGH_COMPLIMENT_MODE_FROM_PWMXA, CUSTOM_DEADTIME, CUSTOM_DEADTIME); //10us
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);                                                                               //Configure PWM0A & PWM0B with above settings
     ESP_LOGI(TAG, "Initialized MCPWM");
     ESP_LOGI(TAG, "Deadtime Enabled");
     while (1)
